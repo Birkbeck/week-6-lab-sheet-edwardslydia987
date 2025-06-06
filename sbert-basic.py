6341403a1a9c5f8aaa9570419dd5f112efa60927
edwardslydia987 <edwardslydia987@gmail.com>
2025-06-06 19:36:05 +0000

    dsfhkj

--- sbert-basic.py
+++ sbert-basic.py
@@ -5,7 +5,7 @@ from sklearn.decomposition import PCA
 
 
 # 1. Load a pretrained Sentence Transformer model
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = SentenceTransformer("all-MiniLM-L6-v2")  
 
 
 with open('data/sentences.txt', 'r', encoding='utf-8') as file:
